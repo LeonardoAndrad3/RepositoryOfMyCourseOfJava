1439ee1e8852d28705ec828de710480a1e01b7a3
LeonardoAndrad3 <leonardoxcontato@gmail.com>
2022-04-30 09:26:35 -0300

    Very bad solution

--- src/exception/Ruim.java
+++ src/exception/Ruim.java
@@ -10,25 +10,24 @@ import model.entites.Reservation;
 
 public class Ruim {
 
-	public static void main(String[] args) throws ParseException{
-		Locale.setDefault(Locale.US);	
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.print("Room number: ");
 		int roomNumber = sc.nextInt();
 		System.out.print("Check-in date (dd/MM/yyyy): ");
 		Date checkIn = sdf.parse(sc.next());
 		System.out.print("Check-out date (dd/MM/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
-		
-		if(!checkOut.after(checkIn)) {
+
+		if (!checkOut.after(checkIn)) {
 			System.out.println("Erro in reservation: Check-out date ust be after check-in date");
-		}
-		else {
+		} else {
 			Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
-			
+
 			System.out.println();
 			System.out.println("Enter data to update the reservation: ");
 			System.out.print("Check-in date (dd/MM/yyyy): ");
@@ -36,13 +35,19 @@ public class Ruim {
 			System.out.print("Check-out date (dd/MM/yyyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			reservation.updateDates(checkIn, checkOut);	
-			System.out.println("Reservation: " + reservation);
+			Date now = new Date();
+			if(checkIn.before(now) || checkOut.before(now)) {
+				System.out.println("Erro in reservation: Reservation dates for update must be future dates");
+			}
+			else if(!checkOut.after(checkIn)) {
+				System.out.println("Erro in reservation: Check-out date ust be after check-in date");
+			}
+			else {
+				reservation.updateDates(checkIn, checkOut);
+				System.out.println("Reservation: " + reservation);
+			}
 		}
-		
-		
-	
-		sc.close();
-	}	
-}
 
+		sc.close();
+	}
+}
